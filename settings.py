39ba3016725180cb144e38a093823aadbeb3a329
David Evans <dave@evans.io>
2019-11-20 15:44:56 +0000

    Replace long error codes with short ones
    There's no need to maintain two separate lists now as there's nothing
    which requires the long versions.

--- settings.py
+++ settings.py
@@ -47,19 +47,6 @@ LINE_COLOUR_CYCLE = [
 
 
 ERROR_CODES = {
-    0: "within range",
-    -1: "under range",
-    1: "over range",
-    2: "with no ref range available",
-    3: "with non-numeric result",
-    4: "with invalid sex",
-    5: "with no ref range calculated - impossible direction",
-    6: "with no ref range calculated for children",
-    7: "with no ref range calculated - invalid ref range",
-}
-
-
-ERROR_CODES_SHORT = {
     0: "Within range",
     -1: "Under range",
     1: "Over range",
